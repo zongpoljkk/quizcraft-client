c1c65fcfd8b3af034ef08bcd9e1e701bf4c09958
nnnnnnnutt <pimkunut.tee@gmail.com>
2020-12-07 21:49:15 +0700

    [C-21] remove unused and move file

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -1,9 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import Homepage from "./Homepage/Homepage";
-import PracticeGame from "./PracticeGame/PracticeGame";
-
 const Page = ({ children }) => {
   return <Container>{children}</Container>;
 };
