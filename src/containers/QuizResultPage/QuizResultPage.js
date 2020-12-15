3634a9a0275b9dc7b745697bf8221057889593fa
Zongpol <levatinez@gmail.com>
2020-12-15 15:53:54 +0700

    WIP

--- src/containers/QuizResultPage/QuizResultPage.js
+++ src/containers/QuizResultPage/QuizResultPage.js
@@ -1,21 +1,33 @@
-import React from "react";
+import { React, useState } from "react";
 import styled from "styled-components";
 
 // Typography
-import { Header } from "../../components/Typography";
+import { Header, Subheader, Body } from "../../components/Typography";
+
+// Color
+import { COLOR } from "../../global/const";
 
 const QuizResultPage = () => {
+  const [exp, setEXP] = useState(0);
+  const [coin, setCoin] = useState(0);
+
   return (
     <Container>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginBottom: "32px",
-        }}
-      >
+      <CenterDiv style={{ marginBottom: "32px" }}>
         <Header>สรุปคะแนน</Header>
-      </div>
+      </CenterDiv>
+
+      <CenterDiv style={{ marginBottom: "64px" }}>
+        <Circle>
+          <Subheader color={COLOR.SILVER}>Out of 10</Subheader>
+        </Circle>
+      </CenterDiv>
+
+      <CenterDiv>
+        <Body color={COLOR.MANDARIN}>
+          คุณได้รับ + {exp} XP + {coin} coins
+        </Body>
+      </CenterDiv>
     </Container>
   );
 };
@@ -24,6 +36,24 @@ const Container = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
+`;
+
+const Circle = styled.span`
+  height: 212px;
+  width: 212px;
+  background-color: ${COLOR.ISLAND_SPICE};
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CenterDiv = styled.div.attrs((props) => ({
+  marginBottom: props.marginBottom,
+}))`
+  display: flex;
+  justify-content: center;
+  margin-bottom: ${(props) => props.marginBottom};
 `;
 
 export default QuizResultPage;
