bdfc9d3e49dbabd8b2f858c91a373a5aebf82dc3
nnnnnnnutt <pimkunut.tee@gmail.com>
2020-12-20 21:14:19 +0700

    [C2-01] reorder path in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
+import backend from "./ip";
 import Navbar from "./components/Navbar";
 import Page from "./containers/Page";
 import ErrorPage from "./containers/ErrorPage/ErrorPage";
@@ -18,11 +20,17 @@ const App = () => {
       <Navbar />
       <Page>
         <Switch>
+        <Route exact path="/:subject/:selected_topic_name/:selected_subtopic_name/:selected_difficulty/practice-game">
+            <PracticeGame />
+          </Route>
+          <Route exact path="/:subject/:selected_topic_name/:selected_subtopic_name/:selected_difficulty/quiz-game">
+            <QuizGame />
+          </Route>
           <Route path="/oauth/mcv-callback">
             <OAuthRedirectPage />
           </Route>
-          <Route exact path="/">
-            <LoginPage />
+          <Route exact path="/:subject/:topic">
+            <SubtopicPage />
           </Route>
           <Route exact path="/homepage">
             <Homepage />
@@ -30,18 +38,12 @@ const App = () => {
           <Route exact path="/topic">
             <TopicPage />
           </Route>
-          <Route exact path="/:subject/:selected_topic_name/:selected_subtopic_name/:selected_difficulty/practice-game">
-            <PracticeGame />
-          </Route>
-          <Route exact path="/:subject/:selected_topic_name/:selected_subtopic_name/:selected_difficulty/quiz-game">
-            <QuizGame />
-          </Route>
-          <Route exact path="/:subject/:topic">
-            <SubtopicPage />
+          <Route exact path="/">
+            <LoginPage />
           </Route>
           <Route path="*">
             <ErrorPage />
-            </Route>
+          </Route>
         </Switch>
       </Page>
     </Router>
