108bca854fa0464d3a9f8baeb7f5f58a9fe4a0a5
nnnnnnnutt <pimkunut.tee@gmail.com>
2020-12-31 17:04:53 +0700

    edit style + add result modal

--- src/containers/ChallengeResultPage/ChallengeResultPage.js
+++ src/containers/ChallengeResultPage/ChallengeResultPage.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
-
-import { Header,Subheader, Body } from "../../components/Typography";
-import { ExitModal } from "../../components/ExitModal";
-import correct_icon from "../../assets/icon/success.png";
-import incorrect_icon from "../../assets/icon/fail.png";
 import { motion } from "framer-motion";
 
-import { COLOR } from "../../global/const"
+import { Header, Subheader } from "../../components/Typography";
+import { ExitModal } from "../../components/ExitModal";
+import { UserInfoBox } from "./components/UserInfoBox";
+import { ResultModal } from "./components/ResultModal";
+import useModal from "../../components/useModal";
+
+
+import { COLOR } from "../../global/const";
 
 //MOCK DATA
 const PROFILE_IMG = "";
-const USERNAME = "pimkunut_tee"
+const USERNAME = "pimkunut_tee";
 const SCORE = "3";
 const RESULT = [1, 1, 0, 1, 0];
 const OPPONENT_PROFILE_IMG = "";
@@ -19,137 +21,107 @@ const OPPONENT_USERNAME = "jinjin";
 const OPPONENT_SCORE = "2";
 const OPPONENT_RESULT = [1, 0, 1, 0, 0];
 
-//CONSTANT
-const NUMBER_OF_PROBLEM = 5;
-
 const ChallengeResultPage = () => {
   const ref = useRef(null);
   const [container_width, set_container_width] = useState();
-  
+  const {isShowing, toggle} = useModal();
+  const [win, set_win] = useState(false);
+  const [showModal, setShowModal] = useState()
+
+  const isWin = () => {
+    //CHECK SCORE
+    if(SCORE > OPPONENT_SCORE){
+      set_win(true);
+    }
+  }
+
   const list = {
     hidden: {
       opacity: 0,
       transition: {
-        when: "afterChildren"
-      }
+        when: "afterChildren",
+      },
     },
     visible: {
       opacity: 1,
       transition: {
         when: "beforeChildren",
-        staggerChildren: 0.5
-      }
-    }
+        staggerChildren: 0.5,
+      },
+    },
   };
 
   const variants = {
-    visible: index => ({
+    visible: (index) => ({
       opacity: 1,
       transition: {
         delay: index * 0.75,
       },
     }),
     hidden: { opacity: 0 },
-  }
+  };
 
   useEffect(() => {
     set_container_width(ref.current ? ref.current.offsetWidth : 0);
   }, [ref.current]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(toggle)
+    }, 7750);
+  }, []);
+
   return (
-    <Container
-      initial="hidden"
-      animate="visible"
+    <Container 
+      initial="hidden" 
+      animate="visible" 
       variants={list} 
       ref={ref}
     >
       <motion.div
-        custom={0} 
-        variants={variants} 
-        style={{display: "flex", alignItems: "center"}}
+        custom={0}
+        variants={variants}
+        style={{ display: "flex", alignItems: "center" }}
       >
         <ExitModal />
-        <div style={{display: "flex", justifyContent: "center", width: container_width-16}}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            width: container_width - 16,
+          }}
+        >
           <Header>สรุปผลคะแนน</Header>
         </div>
       </motion.div>
-      <UserInfoContainer justifyContent={container_width > 411 ? "center": "space-between"}>
-        <UserInfo>
-          <UserImg
-            custom={1} 
-            variants={variants}  
-            backgroundColor={PROFILE_IMG ? null : COLOR.ISLAND_SPICE}
-          >
-              {PROFILE_IMG ? <img src={PROFILE_IMG}/> : null}
-          </UserImg>
-          <motion.div
-            custom={2} 
-            variants={variants} 
-          >
-            <Body> {USERNAME} </Body>
-          </motion.div>
-        </UserInfo>
-        <motion.div
+      <DetailContainer>
+        <UserInfoBox
+          container_width={container_width}
+          profile_image={PROFILE_IMG}
+          username={USERNAME}
+          challenge_result={RESULT}
+          total_score={SCORE}
+        />
+        <motion.div 
           custom={1} 
-          variants={variants}
-          style={container_width > 411 ? {marginLeft:"55px", marginRight: "55px"} : {}}
+          variants={variants} 
+          style={{ marginTop: "9px" }}
         >
           <Subheader props color={COLOR.MANDARIN}>VS</Subheader>
-        </ motion.div>
-        <UserInfo>
-          <UserImg
-            custom={1} 
-            variants={variants}   
-            backgroundColor={OPPONENT_PROFILE_IMG ? null : COLOR.ISLAND_SPICE}
-          >
-              {OPPONENT_PROFILE_IMG ? <img src={OPPONENT_PROFILE_IMG}/> : null}
-          </UserImg>
-          <motion.div
-            custom={2} 
-            variants={variants} 
-          >
-            <Body> {OPPONENT_USERNAME} </Body>
-          </motion.div>
-        </UserInfo>
-      </UserInfoContainer>
-      <ScoreBoxContainer 
-        custom={3} 
-        variants={variants}
-        justifyContent={container_width > 411 ? "center": "space-between"}
-      >
-        <ScoreBox marginRight={container_width > 411 ? "135" : "0"}>
-          {RESULT.map((result, index)=> (
-            <Icon
-              key={index}
-              custom={index+4} 
-              variants={variants}
-              src={result ? correct_icon: incorrect_icon}
-            />
-          ))}
-          <motion.div 
-            custom={NUMBER_OF_PROBLEM+4} 
-            variants={variants} 
-          >
-            <Header props color = {COLOR.MANDARIN}>{SCORE}</Header> 
-          </motion.div>
-        </ScoreBox>
-        <ScoreBox>
-          {OPPONENT_RESULT.map((result, index)=> (
-            <Icon
-              key={index}
-              custom={index+4} 
-              variants={variants} 
-              src={result ? correct_icon: incorrect_icon}
-            />
-          ))}
-          <motion.div 
-            custom={NUMBER_OF_PROBLEM+4} 
-            variants={variants} 
-          >
-            <Header props color = {COLOR.MANDARIN}>{OPPONENT_SCORE}</Header>  
-          </motion.div>
-        </ScoreBox>
-      </ScoreBoxContainer>
+        </motion.div>
+        <UserInfoBox
+          container_width={container_width}
+          profile_image={OPPONENT_PROFILE_IMG}
+          username={OPPONENT_USERNAME}
+          challenge_result={OPPONENT_RESULT}
+          total_score={OPPONENT_SCORE}
+        />
+      </DetailContainer>
+      <ResultModal
+        isShowing={isShowing}
+        toggle={toggle}
+        win={isWin}
+      />
     </Container>
   );
 };
@@ -160,58 +132,11 @@ const Container = styled(motion.div)`
   flex-direction: column;
 `;
 
-const UserInfoContainer = styled.div.attrs(props => ({
-  justifyContent: props.justifyContent
-}))`
+const DetailContainer = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
+  justify-content: space-evenly;
   margin-top: 16px;
-  justify-content: ${props => props.justifyContent};
-`;
-
-const UserInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 120px;
-`;
-
-const UserImg = styled(motion.div).attrs(props => ({
-  backgroundColor: props.backgroundColor
-}))`
-  background-color: ${props => props.backgroundColor};
-  height: 48px;
-  width: 48px;
-  border-radius: 50%;
-  margin-bottom: 8px;
-`;
-
-const ScoreBoxContainer = styled(motion.div).attrs(props => ({
-  justifyContent: props.justifyContent
-}))`
-  display: flex;
-  justify-content: ${props => props.justifyContent};
-`;
-
-const ScoreBox = styled(motion.div).attrs(props => ({
-  marginRight: props.marginRight
-}))`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 24px;
-  margin-right: ${props => props.marginRight}px;
-  padding: 32px 40px 32px 40px;
-  background-color: ${COLOR.ISLAND_SPICE};
-  border-radius: 10px;
-`;
-
-const Icon = styled(motion.img)`
-  alt: "icon";
-  width: 40px;
-  height: 40px;
-  margin-bottom: 24px;
 `;
 
 export default ChallengeResultPage;
---
+++ src/containers/ChallengeResultPage/components/ResultModal.js
@@ -0,0 +1,127 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+import { Header, Body, Overline } from "../../../components/Typography";
+import { Modal } from "../../../components/Modal";
+import { LottieFile } from "../../../components/LottieFile";
+import levelUp from "../../../assets/lottie/levelUp.json";
+import { ProgressBar } from "../../../components/ProgressBar";
+
+import { COLOR } from "../../../global/const";
+
+//MOCK DATA
+const LEVEL = 12;
+const XP = 876;
+const MAX_XP = 2000;
+const GAIN_COIN = 200;
+const GAIN_XP = 150;
+
+export const ResultModal = ({
+  isShowing,
+  toggle,
+  win,
+}) => {
+
+  const list = {
+    hidden: {
+      opacity: 0,
+      transition: {
+        when: "afterChildren"
+      }
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.5
+      }
+    }
+  };
+
+  const item = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 }
+  };
+
+  return (
+    <Modal
+      isShowing={isShowing}
+      hide={toggle}
+    >
+      <Container
+        initial="hidden"
+        animate="visible"
+        variants={list}
+      >
+        <motion.div variants={item}>
+          <Header>{win ? "คุณชนะ!" : "คุณแพ้!"}</Header>
+        </motion.div>
+        <motion.div
+          variants={item} 
+          style = {{marginTop: "4px", marginBottom:"4px" }}
+        >
+          <LottieFile 
+            animationData={levelUp} 
+            width="118px" 
+            height="121px" 
+            loop={false}
+          />
+        </motion.div>
+        <motion.div variants={list}>
+          <Header>+{GAIN_COIN} coins +{GAIN_XP} XP</Header>
+        </motion.div>
+        <ProgressBarContainer variants={list}>
+            <LevelTitleContainer>
+              <LevelTitle marginBottom={6}>
+                <Body>เลเวล</Body>
+                <div style={{ marginRight: 8 }}/>
+                <Body color={COLOR.MANDARIN}>{LEVEL}</Body>
+              </LevelTitle>
+              <LevelTitle marginBottom={2}>
+                <Overline color={COLOR.MANDARIN}>{XP}</Overline>
+                <Overline color={COLOR.SILVER}>/{MAX_XP}</Overline>
+              </LevelTitle>
+            </LevelTitleContainer>
+            <ProgressBar percent={(XP/MAX_XP)*100}/>
+          </ProgressBarContainer>
+      </Container>
+    </Modal>
+  );
+};
+
+const Container = styled(motion.div)`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProgressBarContainer = styled(motion.div)`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 8px;
+`;
+
+const LevelTitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: 100%;
+`;
+
+const LevelTitle = styled.div.attrs(props => ({
+  marginBottom: props.marginBottom
+}))`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: ${props => props.marginBottom}px;
+`;
+
