53bd56e9c7ffedc5601ba3da2c31d5f667d51771
Jinwara <naon123@hotmail.com>
2021-01-09 22:26:24 +0700

    [C4-01] add timerPicker

---
+++ src/components/TimePicker.js
@@ -0,0 +1,95 @@
+import React from "react";
+import styled from "styled-components";
+
+import { Subheader } from "./Typography";
+
+import { useWindowDimensions } from "../global/utils";
+import { COLOR, LARGE_DEVICE_SIZE } from "../global/const";
+
+export const TimePicker = ({
+  value,
+  set_value = () => {}
+}) => {
+
+  return (
+    <TimePickerContainer width={value ? 82 : 64}>
+      <TimePickerComponent
+        value={value}
+        onChange={e => set_value(e.target.value)}
+        color={value ? COLOR.CHARCOAL : COLOR.SILVER }
+      />
+    </TimePickerContainer>
+  );
+};
+
+export const TimePickerWithLabel = ({
+  label,
+  value,
+  set_value,
+  direction = "column",
+  marginBottom
+}) => {
+  const { height, width: screen_width } = useWindowDimensions();
+
+  return (
+    <TimePickerWithLabelContainer
+      flexDirection={direction}
+      justifyContent={direction === "row" ? ( screen_width >= LARGE_DEVICE_SIZE ? null : "space-between" ) : null}
+      marginBottom={marginBottom}
+    >
+      <div style={direction === "column" ? { marginBottom: 8 } : { marginTop: 6, marginRight: 24 }}>
+        <Subheader>{label}</Subheader>
+      </div>
+      <TimePicker
+        value={value}
+        set_value={set_value}
+      />
+    </TimePickerWithLabelContainer>
+  );
+};
+
+const TimePickerWithLabelContainer = styled.div.attrs(props => ({
+  flexDirection: props.flexDirection,
+  justifyContent: props.justifyContent,
+  marginBottom: props.marginBottom
+}))`
+  display: flex;
+  flex-direction: ${props => props.flexDirection};
+  justify-content: ${props => props.justifyContent};
+  margin-bottom: ${props => props.marginBottom}px;
+`;
+
+const TimePickerContainer = styled.div.attrs(props => ({
+  width: props.width,
+  height: props.height || 40
+}))`
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid ${COLOR.SILVER};
+  border-radius: 10px;
+  outline: none;
+  height: ${props => props.height}px;
+  width: ${props => props.width}px;
+  padding: 0px 16px 0px 16px;
+  color: ${COLOR.MANDARIN};
+  &:focus {
+    border-color: ${COLOR.MANDARIN};
+  }
+`;
+
+const TimePickerComponent = styled.input.attrs(props => ({
+  type: "time",
+  color: props.color,
+}))`
+  display: flex;
+  flex: 1;
+  font-family: Prompt, sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  background-color: ${COLOR.WHITE};
+  color: ${props => props.color};
+`;
--- src/containers/CreateGroupPage/CreateGroupPage.js
+++ src/containers/CreateGroupPage/CreateGroupPage.js
@@ -2,11 +2,12 @@ import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import { withRouter } from "react-router-dom";
 
-import { Header, Subheader } from "../../components/Typography";
+import { Header } from "../../components/Typography";
 import { Button } from "../../components/Button";
 import { DropdownWithLabel } from "../../components/Dropdown/Dropdown";
 import { NumberInputSpinnerWithLabel } from "../../components/NumberInputSpinner";
 import { RadioButton } from "../../components/RadioButton";
+import { TimePickerWithLabel } from "../../components/TimePicker";
 
 import { LARGE_DEVICE_SIZE } from "../../global/const";
 import { useWindowDimensions } from "../../global/utils";
@@ -28,6 +29,7 @@ const CreateGroupPage = ({ history }) => {
   const [subtopic, set_subtopic] = useState();
   const [difficulty, set_difficulty] = useState();
   const [number_of_problems, set_number_of_problems] = useState();
+  const [time_per_problem, set_time_per_problem] = useState(null);
   const [is_play, set_is_play] = useState();
   const { height: screen_height, width: screen_width } = useWindowDimensions();
 
@@ -48,6 +50,7 @@ const CreateGroupPage = ({ history }) => {
           label="หัวข้อ"
           value={topic}
           set_value={set_topic}
+          options={options}
           marginBottom={16}
         />
         <DropdownWithLabel
@@ -72,6 +75,13 @@ const CreateGroupPage = ({ history }) => {
           direction="row"
           marginBottom={24}
         />
+        <TimePickerWithLabel
+          label="ระยะเวลาที่ใช้ในแต่ละข้อ"
+          value={time_per_problem}
+          set_value={set_time_per_problem}
+          direction="row"
+          marginBottom={24}
+        />
         <RadioButton 
           value={is_play} 
           selected_value={set_is_play}  
@@ -86,7 +96,11 @@ const CreateGroupPage = ({ history }) => {
         <Button type="outline">
           ยกเลิก
         </Button>
-        <Button onClick={() => { history.push("waiting-room"); }}>
+        {console.log(subject, topic, subtopic, difficulty, number_of_problems, is_play)}
+        <Button
+          // type={subject && }
+          onClick={() => { history.push("waiting-room"); }}
+        >
           สร้าง
         </Button>
       </ButtonContainer>
