93eaa975947c142b3ebf15e1844e278a1100c725
Zongpol <levatinez@gmail.com>
2021-01-11 13:16:11 +0700

    feat: Initiate a page

--- src/containers/AchievementPage/AchievementPage.js
+++ src/containers/AchievementPage/AchievementPage.js
@@ -1,5 +1,53 @@
+import styled from "styled-components";
+
+// Components
+import { Header, Subheader, Overline, Body } from "../../components/Typography";
+import { COLOR } from "../../global/const";
+import { convertHexToRGBA } from "../../global/utils";
+
+// Media
+import BronzeImg from "../../assets/icon/bronze.png";
+import ChevronImg from "../../assets/icon/chevron.png";
+import CloseImg from "../../assets/icon/close.png";
+
 const AchievementPage = () => {
-  return <div>Hello Achievements</div>;
+  return (
+    <Container>
+      <Header>ความสำเร็จ</Header>
+      <AchievementContainer>
+        <AchievementImageContainer>sadas</AchievementImageContainer>
+        sdas
+      </AchievementContainer>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AchievementContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 24px;
+  background-color: ${convertHexToRGBA(COLOR.ISLAND_SPICE, 20)};
+`;
+
+const AchievementImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 80px;
+  background-color: ${COLOR.SILVER};
+  border-radius: 50%;
+`;
 
 export default AchievementPage;
