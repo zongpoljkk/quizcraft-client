6093ad0fffa22e2b598202e84c267f8900caec48
nnnnnnnutt <pimkunut.tee@gmail.com>
2021-01-15 00:25:35 +0700

    [C4-04] edited

--- src/containers/WaitingRoomPage/WaitingRoomPage.js
+++ src/containers/WaitingRoomPage/WaitingRoomPage.js
@@ -25,7 +25,7 @@ const MOCK_DATA = {
       username: "สมชายรักพ่อ"
     },
     {
-      username: "mingmimg"
+      username: "mingming"
     },
     {
       username: "zongpol"
@@ -55,13 +55,15 @@ const MOCK_DATA = {
   number_of_member: 13,
 };
 
-const IS_CREATED = true;
+const IS_CREATOR = false;
 
 const WaitingRoomPage = () => {
   const { height: screen_height, width: screen_width } = useWindowDimensions();
-
+  const COLUMNS = Math.floor((screen_width-96)/110);
+  const GAP = Math.floor((screen_width-(110*COLUMNS)-96)/COLUMNS);
+  
   return (
-    <Container>
+    <Container isCreator = {IS_CREATOR}>
       <div style={{width: "300px", position: "relative"}}>
         <NumberText>
           {MOCK_DATA.number_of_member}
@@ -74,18 +76,21 @@ const WaitingRoomPage = () => {
           />
         </div>
       </div>
-      {IS_CREATED && (
+      {IS_CREATOR && (
         <GroupMemberBox>
-          <div style={{display: "flex", flexFlow: "wrap", justifyContent: "space-between", padding: "16px"}}>
-            {MOCK_DATA.member.slice(0).reverse().map((list, index) => (
-              <div style={{width: "110px", marginBottom: "4px", paddingBottom: screen_width >= LARGE_DEVICE_SIZE ? null : index === MOCK_DATA.member.length-1 ? "16px" : null}}>
-                <Body key={index}> {list.username} </Body>
-              </div>
-            ))}
-          </div>
+            <DisplayGroupMember columns={COLUMNS} gap={GAP}>
+              {MOCK_DATA.member.slice(0).reverse().map((list, index) => (
+                <div 
+                  key={index} 
+                  style={{width: "110px", marginBottom: "4px", paddingBottom: screen_width >= LARGE_DEVICE_SIZE ? null : index === MOCK_DATA.member.length-1 ? "16px" : null}}
+                >
+                  <Body> {list.username} </Body>
+                </div>
+              ))}
+            </DisplayGroupMember>
         </GroupMemberBox>
       )}
-      {IS_CREATED ?
+      {IS_CREATOR ?
         <ButtonContainer justifyContent={screen_width >= LARGE_DEVICE_SIZE ? 'space-evenly' : 'space-between'}>
           <Button type="outline">ยกเลิก</Button>
           <Button>เริ่ม</Button>
@@ -99,12 +104,23 @@ const WaitingRoomPage = () => {
   );
 };
 
-const Container = styled.div`
+const Container = styled.div.attrs((props) => ({
+  isCreator: props.isCreator,
+}))`
   display: flex;
   flex-direction: column;
   flex: 1;
   justify-content: center;
   align-items: center;
+
+  ${(props) => props.isCreator ? null 
+    : `
+      position: fixed;
+      left: 50%;
+      top: 50%;
+      z-index: 1060;
+      transform: translate(-50%, -50%);
+  `}
 `;
 
 const GroupMemberBox = styled.div`
@@ -114,6 +130,16 @@ const GroupMemberBox = styled.div`
   overflow: scroll;
   border-radius: 10px;
   background: ${COLOR.ISLAND_SPICE};
+`;
+
+const DisplayGroupMember  = styled.div.attrs(props => ({
+  columns: props.columns,
+  gap: props.gap
+}))`
+  display: grid;
+  padding: 16px;
+  grid-template-columns: repeat(${props => props.columns}, 1fr);
+  grid-column-gap: ${props => props.gap}px;
 `;
 
 const NumberText = styled.div`
