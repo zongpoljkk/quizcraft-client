66d57c83180e09e9c86493c6180a5a64996d4903
nnnnnnnutt <pimkunut.tee@gmail.com>
2021-01-20 17:37:20 +0700

    completed(?)

--- src/components/ProblemBox.js
+++ src/components/ProblemBox.js
@@ -1,49 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import Tex2SVG, { MathJaxProvider } from "react-hook-mathjax";
-// import * as MathLex from "./mathlex";
+import Tex2SVG from "react-hook-mathjax";
 
 import { Subheader } from "./Typography";
 
-import { COLOR } from "../global/const";
+import { COLOR, CONTAINER_PADDING } from "../global/const";
+import { useWindowDimensions } from "../global/utils";
 
 export const ProblemBox = ({
   problem = '',
   problem_content = '',
 }) => {
 
-  const tex = "(-2)^{-13}*(-2)^{-6}";
-  const tex1 = "\\frac{2}{3}"
-  const tex2 = "(-2)^{-13}*(-2)^{-6}*\\frac{2}{3}*\\frac{4}{5}"
-  const tex3 = "\\frac{(-8)^{9}*(-8)^{29}*(-8)^{-29}}{(-8)^{-32}*(-8)^{38}*(-8)^{20}}"
-  const tex4 = "0.0000000058552"
-  const math = "((-7/6)^[-31]*(-7/6)^[-18]*(-7/6)^[-21]*(-7/6)^[20]*(-7/6)^[19])/((-7/6)^[-18]*(-7/6)^[-18]*(-7/6)^[45])"
-
-  // const MathToLetex = (math) => {
-  //   console.log(math)
-  //   var syntaxTree = MathLex.parse(math).val();
-  //   // var latexCode = MathParser.render(syntaxTree, 'latex');
-  //   // console.log({syntaxTree});
-  //   // console.log({latexCode});
-  //   // return latexCode;
-  //   return syntaxTree;
-  // };
+  const { height: screen_height, width: screen_width } = useWindowDimensions();
+  const asciimath2latex = require("asciimath-to-latex");
 
   return (
     <ProblemContainer>
-      <Subheader>{problem}</Subheader>
-      {problem_content
-        ? <Problem>
-            {/* <Subheader>{problem_content}</Subheader> */}
-            <Subheader>
-              <Tex2SVG display="inline" latex={tex3} />
-            </Subheader>
-            {/* <div>
-              {MathToLetex(math)}
-            </div> */}
+      <ProblemComponent>
+        <Subheader>{problem}</Subheader>
+        {problem_content ? (
+          <Problem width={screen_width - CONTAINER_PADDING - 48}>
+            <Tex2SVG
+              display="inline"
+              latex={asciimath2latex(problem_content)}
+            />
           </Problem>
-        : null
-      }
+        ) : null}
+      </ProblemComponent>
     </ProblemContainer>
   );
 };
@@ -54,9 +38,15 @@ const ProblemContainer = styled.div`
   padding: 24px;
 `;
 
-const Problem = styled.div`
+const ProblemComponent = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+`;
+
+const Problem = styled.image.attrs(props => ({
+  width: props.width
+}))`
+  max-width: ${props => props.width}px;
   margin-top: 16px;
-  text-align: center;
+  align-self: center;
 `;
