13318ef59080444ec00962d4f6330ad244b5bfda
nnnnnnnutt <pimkunut.tee@gmail.com>
2021-01-20 14:14:16 +0700

    test lib

--- src/components/ProblemBox.js
+++ src/components/ProblemBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Tex2SVG, { MathJaxProvider } from "react-hook-mathjax";
+import { MathParser } from "mathlex"
 
 import { Subheader } from "./Typography";
 
@@ -16,6 +17,17 @@ export const ProblemBox = ({
   const tex2 = "(-2)^{-13}*(-2)^{-6}*\\frac{2}{3}*\\frac{4}{5}"
   const tex3 = "\\frac{(-8)^{9}*(-8)^{29}*(-8)^{-29}}{(-8)^{-32}*(-8)^{38}*(-8)^{20}}"
   const tex4 = "0.0000000058552"
+  const math = "((-7/6)^[-31]*(-7/6)^[-18]*(-7/6)^[-21]*(-7/6)^[20]*(-7/6)^[19])/((-7/6)^[-18]*(-7/6)^[-18]*(-7/6)^[45])"
+
+  const MathToLetex = (math) => {
+    console.log(math)
+    var syntaxTree = MathParser.parse(math);
+    // var latexCode = MathParser.render(syntaxTree, 'latex');
+    // console.log({syntaxTree});
+    // console.log({latexCode});
+    // return latexCode;
+    return math;
+  };
 
   return (
     <ProblemContainer>
@@ -23,9 +35,12 @@ export const ProblemBox = ({
       {problem_content
         ? <Problem>
             {/* <Subheader>{problem_content}</Subheader> */}
-            <Subheader>
+            {/* <Subheader>
               <Tex2SVG display="inline" latex={tex3} />
-            </Subheader>
+            </Subheader> */}
+            <div>
+              {MathToLetex(math)}
+            </div>
           </Problem>
         : null
       }
