716bfcbbcd380a022d62f6cf1bb5eca4ed6fe8ac
mingokl02135 <mingbudsakorn@gmail.com>
2021-01-26 18:13:45 +0700

    server sent event test

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -11,6 +11,10 @@ import LoadingPage from "../LoadingPage/LoadingPage";
 
 import { useGetSubjects, useGetLeaderBoard } from "./HomepageHelper";
 
+//--------------------------test-server-sent-event----------------------//
+import { useServerSentEvent, useNextProblem, useGetGroupGame } from "./HomepageHelper";
+//---------------------------------------------------------------------//
+
 const Homepage = ({ history, user_id }) => {
   const ref = useRef(null);
   const [container_width, set_container_width] = useState();
@@ -20,6 +24,30 @@ const Homepage = ({ history, user_id }) => {
     leader_board_loading,
     leader_board,
   } = useGetLeaderBoard(user_id);
+  
+  //--------------------------test-server-sent-event----------------------//
+  const token = localStorage.getItem("token");
+  const groupId = "600ef0055760e624288d02ec";
+  const { 
+    listening, 
+    subscribe, 
+    status_message, 
+    next_problem, 
+    update_member,
+    start_game,
+    send_answer, 
+  } = useServerSentEvent(groupId,token);
+  const { current_problem_index, nextProblem } = useNextProblem(groupId); 
+  const {
+    getGroupGame,
+    loading,
+    current_index,
+    number_of_problem,
+    time_per_problem,
+    user,
+    problem
+  } = useGetGroupGame(user_id, groupId);
+//---------------------------------------------------------------------//
 
   useEffect(() => {
     set_container_width(ref.current ? ref.current.offsetWidth : 0);
@@ -29,6 +57,14 @@ const Homepage = ({ history, user_id }) => {
     getLeaderBoard();
     getSubjects();
   }, []);
+  
+//--------------------------test-server-sent-event----------------------//
+  useEffect(() => {
+    //do something may be request backend
+    //example
+    getGroupGame();
+  }, [next_problem]);
+//---------------------------------------------------------------------//
 
   return (
     <React.Fragment>
@@ -51,6 +87,25 @@ const Homepage = ({ history, user_id }) => {
               <Tabs data={leader_board} />
             </ItemBox>
           </div>
+
+          {
+            //--------------------------test-server-sent-event----------------------//
+          }
+        <div>
+          <p>{listening ? status_message.subscribed : status_message.unsubscribed}</p>
+          <button onClick={subscribe}>
+            {listening ? status_message.unsubscribed : status_message.subscribed}
+          </button>
+          <button onClick={nextProblem}> Next Problem
+          </button>
+          <p>current_problem_index form nextProblem(): {current_problem_index}</p>
+          <p>next_problem_message: {next_problem}</p>
+          <p>current_index from getGroupGame(): {current_index}</p>
+        </div>
+          {
+            //---------------------------------------------------------------------//
+          }
+
         </Container>
       )}
     </React.Fragment>
--- src/containers/Homepage/HomepageHelper.js
+++ src/containers/Homepage/HomepageHelper.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
 import backend from "../../ip";
+//--------------------------test-server-sent-event----------------------//
+import EventSource from "eventsource"
 
 export const useGetSubjects = () => {
   const [subjects, set_subjects] = useState([]);
@@ -49,3 +51,128 @@ export const useGetLeaderBoard = (user_id) => {
 
   return { getLeaderBoard, leader_board_loading, leader_board };
 };
+
+
+//---------------------------------test-server-sent-event-----------------------------//
+export const useServerSentEvent = (groupId, token) => {
+  const [listening, set_listening] = useState(false);
+  const [next_problem, set_next_problem] = useState();
+  const [start_game, set_start_game] = useState();
+  const [update_member, set_update_member] = useState();
+  const [restart_game, set_restart_game] = useState();
+  const [send_answer, set_send_answer] = useState();
+
+  const status_message = {
+    subscribed: "Subscribed",
+    unsubscribed: "Unsubscribed"
+  };
+
+  const subscribe = async () => {
+    const status = listening;
+    if (!status) {
+      //subscribe (when join group)
+      const events = new EventSource(`${backend}group/event?groupId=${groupId}`, {
+        headers: {
+          'Authorization': 'Bearer ' + token
+        }
+      });
+      events.onmessage = event => {
+        const parsedData = JSON.parse(event.data);
+        console.log(event)
+        //example
+        switch (parsedData.type) {
+          case "INIT_CONNECTION":
+            console.log("init")
+            break;
+          case "NEXT_PROBLEM":
+            set_next_problem(parsedData.message);
+            console.log("next_problem");
+            break;
+          case "START_GAME":
+            set_start_game(parsedData.message);
+            console.log("start_game");
+            break;
+          case "UPDATE_MEMBER":
+            set_update_member(parsedData.message);
+            console.log("update_member");
+            break;
+          case "RESTART_GAME":
+            set_restart_game(parsedData.message);
+            console.log("restart_game");
+            break;
+          case "SEND_ANSWER":
+            set_send_answer(parsedData.message);
+            console.log("send_answer");
+            break;
+        }
+      };
+    } else {
+      //unsubscribe (when leave group -> waiting room, game result)
+      await axios.delete(`${backend}group/close/`);
+    }
+    set_listening(!listening);
+  };
+  return { listening, subscribe, status_message, next_problem, update_member, start_game, restart_game, send_answer };
+}
+
+export const useNextProblem = (groupId) => {
+  const [current_problem_index,set_current_problem] = useState();
+
+  const nextProblem = async () => {
+    try {
+      const response = await axios.post(
+        backend + "group/next-problem",
+        {
+          groupId: groupId,
+        }
+      );
+      const { success, data } = response.data;
+      if (success) {
+        set_current_problem(data.currentIndex);
+      } else {
+        console.log("next problem Error");
+      }
+    } catch (e) {
+      console.log("There are something wrong about next problem  :(");
+    }
+  }
+
+  return { current_problem_index, nextProblem };
+}
+
+export const useGetGroupGame = (user_id, group_id) => {
+  const [current_index, set_current_index] = useState();
+  const [number_of_problem, set_number_of_problem] = useState();
+  const [time_per_problem, set_time_per_problem] = useState();
+  const [user, set_user] = useState();
+  const [problem, set_problem] = useState();
+  const [loading, set_loading] = useState(true);
+
+  const getGroupGame = async () => {
+    set_loading(true);
+    try {
+      const response = await axios.get(backend + "group/get-group-game", {
+        params: {
+          userId: user_id,
+          groupId: group_id
+        }
+      });
+      const { success, data } = response.data;
+      if (success) {
+        set_current_index(data.currentIndex);
+        set_number_of_problem(data.numberOfProblem);
+        set_time_per_problem(data.timePerProblem);
+        set_user(data.user);
+        set_problem(data.problem);
+        set_loading(false);
+      } else {
+        console.log("getGroupGame Error");
+      } 
+    } catch (error) {
+      console.log("There are something wrong about get group game :(");
+    }
+  };
+
+  return { getGroupGame, loading, current_index, number_of_problem, time_per_problem, user, problem };
+};
+//-----------------------------------------------------------------------------------//
