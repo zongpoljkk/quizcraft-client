4a0e48e00d46c91fd1d4e867eea8789e1972502c
nnnnnnnutt <pimkunut.tee@gmail.com>
2021-02-01 13:43:19 +0700

    supposed to work

--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Redirect, useLocation } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import axios from "axios";
@@ -13,73 +13,48 @@ export const PrivateRoute = ({
   
   let token = localStorage.getItem("token");
   const location = useLocation();
+  const { exp } = jwt_decode(token);
+  const [refresh_token, set_refresh_token] = useState(0);
 
-    // const refreshToken = async () => {
-  //   try {
-  //     const response = await axios.post(backend + "auth/refresh-token");
-  //     const { success, data } = response.data;
-  //     if (success) {
-  //       localStorage.setItem("token", data.token);
-  //       axios.defaults.headers.common[
-  //         "Authorization"
-  //       ] = `Bearer ${response.data.token}`;
-  //     } else {
-  //       console.log("refreshToken Error");
-  //     }
-  //   } catch (error) {
-  //     console.log("There are something wrong about get refreshToken :(");
-  //   }
-  // };
-
-    // axios.interceptors.request.use(
-    //   async (config) => {
-    //     const { exp } = jwt_decode(token);
-    //     if (exp * 1000 - Date.now() <= 900000) {
-    //       await refreshToken();
-    //       return config;
-    //     } else {
-    //       return config;
-    //     }
-    //   },
-    //   (err) => {
-    //     return Promise.reject(err);
-    //   }
-    // );
+    const refreshToken = async () => {
+    try {
+      const response = await axios.post(backend + "auth/refresh-token");
+      const { success, token } = response.data;
+      if (success) {
+        localStorage.setItem("token", token);
+        axios.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${token}`;
+      } else {
+        console.log("refreshToken Error");
+      }
+    } catch (error) {
+      console.log(error)
+      console.log("There are something wrong about get refreshToken :(");
+    }
+  };
 
   axios.interceptors.response.use(
     response => {
+      if (exp * 1000 - Date.now() < 900000) {
+        set_refresh_token(refresh_token+1);
+      } 
       return response;
     },
     error => {
-
-      const { exp } = jwt_decode(token);
-
       if (exp * 1000 - Date.now() > 900000) {
         return Promise.reject(error);
       }
-  
-      return axios
-        .post(backend + "auth/refresh-token")
-        .then(response => {
-          localStorage.setItem("token", response.data.token);
-          axios.defaults.headers.common["Authorization"] = `Bearer ${
-            response.data.token
-          }`;
-
-          error.hasRefreshedToken = true;
-          return Promise.reject(error);
-        })
-        .catch(() => {
-          const tokenError = new Error("Cannot refresh token");
-          tokenError.originalError = error;
-          return Promise.reject(tokenError);
-        });
     }
   );
 
   useEffect(() => {
     getUserData();
   }, [location]);
+
+  useEffect(async () => {
+    await refreshToken();
+  }, [refresh_token]);
 
   return (
     <Route
