efd4be324f4fd329218d033eb1e18baacf6dd838
nnnnnnnutt <pimkunut.tee@gmail.com>
2021-02-01 13:45:13 +0700

    edit useEffect

--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -5,43 +5,36 @@ import axios from "axios";
 
 import backend from "../ip";
 
-export const PrivateRoute = ({ 
-  children,
-  getUserData = () => {},
-  ...rest
-}) => {
-  
+export const PrivateRoute = ({ children, getUserData = () => {}, ...rest }) => {
   let token = localStorage.getItem("token");
   const location = useLocation();
   const { exp } = jwt_decode(token);
   const [refresh_token, set_refresh_token] = useState(0);
 
-    const refreshToken = async () => {
+  const refreshToken = async () => {
     try {
       const response = await axios.post(backend + "auth/refresh-token");
       const { success, token } = response.data;
       if (success) {
         localStorage.setItem("token", token);
-        axios.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${token}`;
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
       } else {
         console.log("refreshToken Error");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
       console.log("There are something wrong about get refreshToken :(");
     }
   };
 
   axios.interceptors.response.use(
-    response => {
+    (response) => {
       if (exp * 1000 - Date.now() < 900000) {
-        set_refresh_token(refresh_token+1);
-      } 
+        set_refresh_token(refresh_token + 1);
+      }
       return response;
     },
-    error => {
+    (error) => {
       if (exp * 1000 - Date.now() > 900000) {
         return Promise.reject(error);
       }
@@ -52,8 +45,8 @@ export const PrivateRoute = ({
     getUserData();
   }, [location]);
 
-  useEffect(async () => {
-    await refreshToken();
+  useEffect(() => {
+    refreshToken();
   }, [refresh_token]);
 
   return (
