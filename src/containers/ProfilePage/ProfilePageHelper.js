0302dac25b29e4dd2096d39c896edc849d599e34
mingokl02135 <mingbudsakorn@gmail.com>
2021-02-04 18:09:05 +0700

    refresh when change success

--- src/containers/ProfilePage/ProfilePageHelper.js
+++ src/containers/ProfilePage/ProfilePageHelper.js
@@ -1,15 +1,10 @@
-import React, { useState } from "react";
 import axios from "axios";
 
 import backend from "../../ip";
 
 export const useChangeProfileImage = () => {
-  const [loading, set_loading] = useState(false);
-  // const [group_id, set_group_id] = useState();
-  // const [join_fail, set_join_fail] = useState();
 
   const changeProfileImage = async (formData) => {
-    set_loading(true);
     try {
       const response = await axios.put(
         backend + "user/change-profile-picture",
@@ -22,8 +17,7 @@ export const useChangeProfileImage = () => {
       );
       const { success, data } = response.data;
       if (success) {
-        set_loading(false);
-        // toggle();
+        window.location.reload();
       } else {
         console.log("Change Profile Image Error");
       }
@@ -32,38 +26,6 @@ export const useChangeProfileImage = () => {
     }
   };
 
-  return { changeProfileImage, loading};
+  return { changeProfileImage };
 };
 
-// export const useJoinGroup = () => {
-//   const [loading, set_loading] = useState(false);
-//   const [group_id, set_group_id] = useState();
-//   const [join_fail, set_join_fail] = useState();
-
-//   const joinGroup = async (user_id, pin, toggle) => {
-//     set_loading(true);
-//     try {
-//       const response = await axios.put(backend + "group/join-group", {
-//         userId: user_id,
-//         pin: pin
-//       });
-//       const { success, data } = response.data;
-//       if (success) {
-//         set_group_id(data.groupId);
-//         set_loading(false);
-//         toggle();
-//       } else {
-//         console.log("joinGroup Error");
-//       }
-//     } catch (error) {
-//       if(error.response.status === 400){
-//         set_join_fail(error.response.data.error);
-//         set_loading(false);
-//         toggle();
-//       }
-//       console.log("There are something wrong about joining group  :(");
-//     }
-//   };
-
-//   return { joinGroup, loading, group_id, join_fail };
-// };
