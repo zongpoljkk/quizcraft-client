4b13d5b365b81ae9dd3586313d9ba20a7f90345f
nnnnnnnutt <pimkunut.tee@gmail.com>
2021-02-05 16:13:46 +0700

    delete unused

--- src/containers/ReportPage/ReportPageHelper.js
+++ src/containers/ReportPage/ReportPageHelper.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import backend from "../../ip";
 
 export const useSendReport = (user_id, problem_id, report_content, date) => {
-  const [report_success, set_report_success] = useState();
+  const [report_success, set_report_success] = useState(false);
 
   const sendReport = async () => {
     try {
@@ -16,13 +16,11 @@ export const useSendReport = (user_id, problem_id, report_content, date) => {
       })
       const { success, data } = response.data;
       if (success) {
-        set_report_success(success);
+        set_report_success(true);
       } else {
-        set_report_success(false);
         console.log("send report Error");
       } 
     } catch (error) {
-      set_report_success(false);
       console.log("There are something wrong about send report :(");
     }
   };
