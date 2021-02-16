a6f75adce9419f040007b66e24fcae9f9cc0f77a
Zongpol <levatinez@gmail.com>
2021-02-16 21:27:53 +0700

    chore: Remove debug console.log

--- src/global/achievement.js
+++ src/global/achievement.js
@@ -13,7 +13,6 @@ export const checkAchievement = async (user_id, type, streaks = null) => {
     );
     const { success, data } = response.data;
     if (success) {
-      console.log(data)
       return data;
     } else {
       console.log("checkAchievement Error");
